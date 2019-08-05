3dfa937fc7a4744ecbf063d233d77fe9711f6428
Robert Klotzner <robert.klotzner@gmx.at>
2019-08-05 10:24:56 +0200

    Initial commit.
    Type checks, but untested.

---
+++ README.md
@@ -0,0 +1,8 @@
+= Library for checking passwords against the haveibeenpwned.com database =
+
+By means of this library you can do some basic strength check on new user
+passwords. Common weak passwords like many plain English words or also many
+stronger passwords which happen to have been leaked will likely be found in the
+database and can thus be rejected.
+
+
---
+++ src/HaveIBeenPwned.hs
@@ -0,0 +1,152 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PackageImports #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+-- | Query haveibeenpwned database to check basic password strength in a secure way.
+--
+--   By checking new user passwords against a database of leaked passwords you
+--   get some means for rejecting very weak or just leaked passwords.
+module HaveIBeenPwned where
+
+import "cryptohash" Crypto.Hash -- or maybe i wanted cryptonite?
+import Control.Monad.Reader
+import Control.Monad.Logger
+import Control.Exception
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Lazy as LBS
+import Data.Text(Text)
+import Data.Text.Encoding(encodeUtf8)
+import qualified Data.Text.Lazy.Encoding
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS
+import Network.HTTP.Types.Status(Status(..))
+import System.IO
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import Data.Default (def)
+import Safe (readMay)
+
+data HaveIBeenPwnedError
+  = HaveIBeenPwnedError_BadResponse Status
+  | HaveIBeenPwnedError_NetworkError HttpException
+  deriving (Show) -- no eq instance for HttpException
+
+data HaveIBeenPwnedConfig = HaveIBeenPwnedConfig
+  { _haveIBeenPwnedConfig_manager :: Manager
+  , _haveIBeenPwnedConfig_apihost :: Text
+  }
+
+-- | Result of a password check.
+data HaveIBeenPwnedResult
+  = HaveIBeenPwnedResult_Disclosed Int
+    -- ^ How many times the password was found in public places.  0 means
+    -- obviously none, thus the password is considered "secure", as far as the
+    -- database can tell.
+  | HaveIBeenPwnedResult_ApiError
+    -- ^ The check failed for some reason.
+  deriving (Eq, Ord, Show)
+
+class Monad m => MonadPwned m where
+  -- | Returns the number of disclosures the supplied password has been seen in.
+  --
+  -- If this is not zero, do not use the supplied password, it is known to hackers.
+  -- If it *is* zero, it might still not be safe, only that if it is
+  -- compromised, that is not yet known.
+  --
+  -- https://haveibeenpwned.com/API/v2#SearchingPwnedPasswordsByRange
+  haveIBeenPwned :: Text -> m HaveIBeenPwnedResult
+
+newtype PwnedT m a = PwnedT { unPwnedT :: ReaderT HaveIBeenPwnedConfig m a }
+  deriving (Functor, Applicative, Monad , MonadIO, MonadLogger
+    , MonadTrans
+    )
+
+runPwnedT :: PwnedT m a -> HaveIBeenPwnedConfig -> m a
+runPwnedT (PwnedT (ReaderT f)) = f
+
+mapPwnedT :: (m a -> n b) -> PwnedT m a -> PwnedT n b
+mapPwnedT f = PwnedT . mapReaderT f . unPwnedT
+
+instance MonadReader r m => MonadReader r (PwnedT m) where
+  ask = lift ask
+  local = mapPwnedT . local
+  reader = lift . reader
+
+instance (MonadLogger m, MonadIO m) => MonadPwned (PwnedT m) where
+ haveIBeenPwned password = do
+  let (pfx, rest) = passwdDigest password
+  cfg <- PwnedT ask
+  let request = parseRequest_ $ T.unpack $ T.concat [_haveIBeenPwnedConfig_apihost cfg, "/", pfx]
+  result' <- liftIO $ try $ httpLbs request (_haveIBeenPwnedConfig_manager cfg)
+  case result' of
+    Left err -> do
+      $(logError) $ T.pack $ show @ HttpException $ err
+      return HaveIBeenPwnedResult_ApiError
+    Right result -> case responseStatus result of
+      Status 200 _ -> case parseHIBPResponse (responseBody result) rest of
+        Just n -> pure $ HaveIBeenPwnedResult_Disclosed n
+        Nothing -> do
+          $(logError) $ "Parsing number of occurrences failed. (Not an Int)."
+          pure HaveIBeenPwnedResult_ApiError
+      Status code phrase -> do
+        $(logError) $ T.pack $ show $ Status code phrase
+        return HaveIBeenPwnedResult_ApiError
+
+
+-- | Get the sha1 digest for the supplied password, split into two parts, to agree with the
+--   hibp api.
+passwdDigest :: Text -> (Text, Text)
+passwdDigest passwd = (T.take 5 digest, T.drop 5 digest)
+  where digest = T.toUpper $ T.pack $ show $ sha1 $ encodeUtf8 passwd
+        sha1 :: ByteString -> Digest SHA1
+        sha1 = hash
+
+-- | The hibp response is a line separated list of colon separated hash
+-- *suffixes* and a number indicating the number of times that password(hash)
+-- has been seen in known publicly disclosed leaks
+parseHIBPResponse :: LBS.ByteString -> Text -> Maybe Int
+parseHIBPResponse response suffix =
+  let
+    digests :: [(LT.Text, Maybe Int)]
+    digests = fmap (fmap (readMay . LT.unpack . LT.drop 1) . LT.breakOn ":") $ LT.lines $ Data.Text.Lazy.Encoding.decodeUtf8 response
+  in case filter ((LT.fromStrict suffix ==) . fst) digests of
+    ((_,n):_) -> n
+    [] -> Just 0
+
+-- a really simple demo of the hibp functionality
+consoleHaveIBeenPwned :: IO ()
+consoleHaveIBeenPwned = do
+  runStdoutLoggingT $ do
+    mgr <- liftIO $ newManager tlsManagerSettings
+    p <- liftIO $ getPassword
+    let hibpEnv = HaveIBeenPwnedConfig mgr "https://api.pwnedpasswords.com/range"
+    p' <- flip runPwnedT hibpEnv $ haveIBeenPwned $ T.pack p
+    liftIO $ case p' of
+      HaveIBeenPwnedResult_Disclosed 0 ->
+        putStrLn "Your password does not appear in any known breaches.  Practice good password hygene."
+      HaveIBeenPwnedResult_Disclosed p'' ->
+        putStrLn $ "You have been pwned!  your password has appeared in breaches " ++ show p'' ++ " times."
+      HaveIBeenPwnedResult_ApiError ->
+        putStrLn "Network Error, try again later"
+
+getPassword :: IO String
+getPassword = do
+  putStr "Password: "
+  hFlush stdout
+  password <- withEcho False getLine
+  putChar '\n'
+  return password
+
+withEcho :: Bool -> IO a -> IO a
+withEcho echo action = do
+  old <- hGetEcho stdin
+  bracket_ (hSetEcho stdin echo) (hSetEcho stdin old) action
